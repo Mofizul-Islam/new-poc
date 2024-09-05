8404b973250ddafcd99f5e82edbd7b5c4610466d
Mofizul Islam <92608521+Mofizul-Islam@users.noreply.github.com>
2024-09-05 22:28:14 +0530

    some changes

--- src/pages/QAndA.jsx
+++ src/pages/QAndA.jsx
@@ -13,27 +13,13 @@ import {
   Modal,
   TextField,
 } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/DeleteOutlined";
-import SaveIcon from "@mui/icons-material/Save";
 import axios from "axios";
-import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
-
-import {
-  GridRowModes,
-  DataGrid,
-  GridActionsCellItem,
-  GridRowEditStopReasons,
-} from "@mui/x-data-grid";
-import Radio from "@mui/material/Radio";
-// import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
 import { useLocation } from "react-router-dom";
 import { PDFViewer } from "@react-pdf/renderer";
 import QuestionPaperPdf from "./QuestionPaperPdf";
 import { BASE_URL } from "../api/constants";
+import { TestList } from "../components/qAndA/TestList";
 // import ReactPDF from "@react-pdf/renderer";
 
 const roles = ["Market", "Finance", "Development"];
@@ -157,9 +143,9 @@ export default function QAndA() {
     setActiveTestQuestions(questions);
     setQuestionPDFOpen(true);
   };
-  const questionAndanswer = ()=>{
-    return state?.doc?.doc_name
-  }
+  const questionAndanswer = () => {
+    return state?.doc?.doc_name;
+  };
 
   console.log("ROWS", rows);
 
@@ -267,113 +253,11 @@ export default function QAndA() {
           </Grid>
         </Box>
       </Paper>
-      <Paper>
-        <Box
-          sx={{
-            height: 500,
-            width: "100%",
-            "& .actions": {
-              color: "text.secondary",
-            },
-            "& .textPrimary": {
-              color: "text.primary",
-            },
-          }}
-        >
-          <DataGrid
-            sx={{
-              ".MuiDataGrid-columnHeaderTitle": {
-                fontWeight: 600,
-              },
-            }}
-            // rows={initialRows}
-            rows={rows}
-            columns={[
-              {
-                // field: state.doc.doc_name,
-                valueGetter: questionAndanswer,
-                headerName: "Questions And Answers ",
-                width: 190,
-                editable: true,
-              },
-              {
-                field: "type",
-                headerName: "Type",
-                type: "Pdf",
-                width: 100,
-                align: "left",
-                headerAlign: "left",
-                editable: true,
-              },
-              {
-                field: "generated_date",
-                headerName: "Generate Date",
-                type: "text",
-                width: 120,
-                editable: true,
-              },
-              {
-                field: "subject",
-                headerName: "Subject",
-                width: 120,
-                editable: true,
-                type: "singleSelect",
-              },
-              {
-                field: "grade",
-                headerName: "Grade",
-                width: 120,
-                editable: true,
-                type: "singleSelect",
-              },
-              {
-                field: "test",
-                headerName: "Test",
-                width: 150,
-                editable: true,
-                type: "singleSelect",
-              },
-              {
-                field: "actions",
-                type: "actions",
-                headerName: "Actions",
-                width: 230,
-
-                cellClassName: "actions",
-                getActions: ({ id }) => {
-                  return [
-                    <GridActionsCellItem
-                      icon={<EditIcon />}
-                      label="Edit"
-                      className="textPrimary"
-                      color="inherit"
-                    />,
-                    <GridActionsCellItem
-                      icon={<DeleteIcon />}
-                      label="Delete"
-                      color="inherit"
-                    />,
-                    <GridActionsCellItem
-                      icon={<PictureAsPdfIcon />}
-                      onClick={() => handlePDFGeneration(id)}
-                      label="pdf"
-                      color="inherit"
-                    />,
-                    // <Button variant={"contained"}>Generate Pdf</Button>,
-                  ];
-                },
-              },
-            ]}
-            editMode="row"
-            onRowEditStop={(params, event) => {
-              if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-                event.defaultMuiPrevented = true;
-              }
-            }}
-            processRowUpdate={(newRow) => newRow}
-          />
-        </Box>
-      </Paper>
+      <TestList
+        rows={rows}
+        handlePDFGeneration={handlePDFGeneration}
+        docName={state?.doc?.doc_name}
+      />
       <Modal
         open={questionPDFOpen}
         onClose={() => setQuestionPDFOpen(false)}
